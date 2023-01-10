6d53a17f942f4071710a5d7d4a5e1738d8d06046
jenna-lab <jennifer.sammy@student.moringaschool.com>
2023-01-10 16:12:04 +0300

    completed assignment

--- src/ColoradoStateParks.js
+++ src/ColoradoStateParks.js
@@ -1,8 +1,13 @@
 import React from "react";
 import howManyParks from "./parks/howManyParks";
+import MesaVerde from "./parks/MesaVerde";
+
 
 function ColoradoStateParks() {
   howManyParks(); // => "42 parks!"
 
   return <h1>Colorado State Parks!</h1>;
 }
+<MesaVerde/>
+
+export default ColoradoStateParks
